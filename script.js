a69d40eea65ede8f99869c0ba2562d7d775248c1
deepauraconnect-commit <deepaura.connect@gmail.com>
2026-03-28 09:01:51 -0700

    script.js

--- script.js
+++ script.js
@@ -24,20 +24,31 @@ function toggleFAQ(el) {
     let p = el.nextElementSibling;
     p.style.display = (p.style.display === "block") ? "none" : "block";
 }
+// ================= google form=================
+function openContact(){
+    window.open("https://docs.google.com/forms/d/e/1FAIpQLScbl77jMf0BOQhcjck4LfJuTObH7WjC0JDgH8Ca_jXrG7nSbw/viewform", "_blank");
+}
 
+// ================= HERO BACKGROUND SLIDER =================
+const images = [
+    "https://images.unsplash.com/photo-1556157382-97eda2d62296",
+    "https://images.unsplash.com/photo-1492724441997-5dc865305da7",
+    "https://images.unsplash.com/photo-1522202176988-66273c2fd55f",
+    "https://images.unsplash.com/photo-1519389950473-47ba0277781c"
+];
 
-// ================= EMAILJS =================
-(function(){
-    emailjs.init("YOUR_PUBLIC_KEY");  // अपना key डालना
-})();
+let index = 0;
 
-document.getElementById("contactForm")?.addEventListener("submit", function(e) {
-    e.preventDefault();
+function changeBackground() {
+    index++;
+    if (index >= images.length) {
+        index = 0;
+    }
 
-    emailjs.sendForm("YOUR_SERVICE_ID", "YOUR_TEMPLATE_ID", this)
-    .then(function() {
-        alert("Message Sent Successfully!");
-    }, function(error) {
-        alert("Failed to send message.");
-    });
-});
+    document.querySelector(".hero").style.background =
+        `linear-gradient(rgba(0,0,0,0.6), rgba(0,0,0,0.7)),
+        url('${images[index]}') center/cover no-repeat`;
+}
+
+// change image every 4 seconds
+setInterval(changeBackground, 4000);
